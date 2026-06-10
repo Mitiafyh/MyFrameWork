b0f4b56a9007aa591e60d4c4d2a83a7260b569d2
Mitiafyh <mitiafiorenantsoa@gmail.com>
2026-06-10 07:57:41 +0300

    servlet

---
+++ src/main/java/Presentation/FrontControllerServlet.java
@@ -0,0 +1,28 @@
+package main.java.Presentation;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class FrontControllerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uri = request.getRequestURI();
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println(uri);
+
+    }
