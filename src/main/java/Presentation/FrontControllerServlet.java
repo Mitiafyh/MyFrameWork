f5057cf0300a48f8b47ff2c7e00a238735c1b2ee
Mitiafyh <mitiafiorenantsoa@gmail.com>
2026-06-19 16:33:23 +0300

    sprint2 Url_Mapping

--- src/main/java/Presentation/FrontControllerServlet.java
+++ src/main/java/Presentation/FrontControllerServlet.java
@@ -7,24 +7,26 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
 
 import Utils.*;
 
 public class FrontControllerServlet extends HttpServlet {
-    
+
     private List<Class<?>> classesControllers;
+
+        Utilitaire utilitaire = new Utilitaire();
 
     @Override
     public void init() throws ServletException {
         String packageCible = getInitParameter("packageToScan");
 
-        if(packageCible == null || packageCible.isEmpty()){
+        if (packageCible == null || packageCible.isEmpty()) {
             System.out.println("Erreur : Le paramètre 'packageToScan' n'est pas configuré dans le web.xml !");
             return;
         }
 
-        this.classesControllers = Utilitaire.getClassesWithAnnotation(packageCible, annotation.Controller.class);
-        
+        this.classesControllers = utilitaire.getClassesWithAnnotation(packageCible, annotation.Controller.class);
 
     }
 
@@ -40,28 +42,42 @@ public class FrontControllerServlet extends HttpServlet {
 
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uri = request.getRequestURI();
+        String path = request.getPathInfo();
 
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println(uri + "<br>");
+
         out.println("\n");
         out.println("\n");
 
-        out.println("Classes avec annotation @Controller : "+ "<br>");
+        out.println("Classes avec annotation @Controller : " + "<br>");
 
-        if(this.classesControllers != null){
-            for(Class<?> classe : this.classesControllers){
-                out.println(" - " + classe.getName()+ "<br>");
-
-            }
-        }else{
-                out.println(" Pas de classes avec l'annotation " );
+        if (this.classesControllers != null) {
+            for (Class<?> classe : this.classesControllers) {
+                out.println(" - " + classe.getName() + "<br><br>");
 
             }
-    
+        } else {
+            out.println(" Pas de classes avec l'annotation ");
+
+        }
+
+        Map<String, String> url = utilitaire.GetUrl(this.classesControllers, annotation.UrlMapping.class, path);
+        if (url != null && !url.isEmpty()) {
+            out.println("Cet URL existe, voici les informations de l'URL : " + "<br>");
+            out.println("URL: " + url.get("URL") + "  Class: " + url.get("className") + "  Method: " + url.get("NomMethode") + "<br>");
+        } else {
+
+            out.println("Cet URL n'existe pas, voici les URL existant");
+            List<Map<String, String>> url1 = utilitaire.GetAllUrl(this.classesControllers, annotation.UrlMapping.class);
+
+
+            for (Map<String, String> url2 : url1) {
+                out.println("URL: " + url2.get("URL") + "  Class: " + url2.get("className") + "  Method: " + url2.get("NomMethode") + "<br>");
+            }
+
+        }
 
     }
-
-   
-
 }
--- src/main/java/Utils/Utilitaire.java
+++ src/main/java/Utils/Utilitaire.java
@@ -1,14 +1,93 @@
 package Utils;
 
+import annotation.UrlMapping;
 import java.io.File;
-import java.lang.annotation.Annotation;
+import java.lang.annotation.*;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Utilitaire {
+    // public static void afficher(List<Class<?>> listeclasse,Class<? extends Annotation> annotationClass, String url){
+    //     boolean find = false;
+    //     String affiche = "";
+    //     for (Class classe : listeclasse){
+    //         for(Method methode : getAllMethodeAnnote(classe,annotationClass)){
+    //             if(methode.getAnnotation(annotationClass).equals(url)){
+    //                 find = true;
+    //                  affiche = url +"   " + classe.getName()+"   "+methode.getName();
+    //                break;
+    //             }
+    //         }
+    //     }
+    //     if(find==true){
+    //         System.out.println(affiche);
+    //     }else{
+    //         System.out.println("les methode existant: ");
+    //         for (Class<?> classe : listeclasse){
+    //         for(Method methode : getAllMethodeAnnote(classe,annotationClass)){
+    //             System.out.println(methode.getAnnotation(annotationClass)+"   "+  classe.getName()+"   "+methode.getName());
+    //         }
+    //     }
+    //     }
+    // }
 
-    public static List<Class<?>> getClassesWithAnnotation(String NomPackage, Class<? extends Annotation> annotationClass) {
+    public List<Map<String, String>> GetAllUrl(List<Class<?>> listeclasse, Class<? extends Annotation> annotationClass) {
+        List<Map<String, String>> methode = new ArrayList<>();
+        if (listeclasse == null) {
+            return methode;
+        }
+
+        for (Class<?> classe : listeclasse) {
+            for (Method method : getAllMethodeAnnote(classe, annotationClass)) {
+                UrlMapping urlMapping = method.getAnnotation(UrlMapping.class);
+                String url = urlMapping.value();
+                Map<String, String> methodel = new HashMap<>();
+                methodel.put("className", classe.getSimpleName());
+                methodel.put("NomMethode", method.getName());
+                methodel.put("URL", url);
+                methode.add(methodel);
+            }
+        }
+        return methode;
+    }
+
+    public Map<String, String> GetUrl(List<Class<?>> listeclasse, Class<? extends Annotation> annotationClass, String url) {
+        if (listeclasse == null || url == null) {
+            return null;
+        }
+
+        for (Class<?> classe : listeclasse) {
+            for (Method method : getAllMethodeAnnote(classe, annotationClass)) {
+                UrlMapping urlMapping = method.getAnnotation(UrlMapping.class);
+                String urlAnnotation = urlMapping.value();
+                if (urlAnnotation.equals(url)) {
+                    Map<String, String> methodel = new HashMap<>();
+                    methodel.put("className", classe.getSimpleName());
+                    methodel.put("NomMethode", method.getName());
+                    methodel.put("URL", urlAnnotation);
+                    return methodel;
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<Method> getAllMethodeAnnote(Class<?> classe, Class<? extends Annotation> annotationClass) {
+        List<Method> listeMethode = new ArrayList<>();
+        Method[] methods = classe.getDeclaredMethods();
+        for (Method methode : methods) {
+            if (methode.isAnnotationPresent(annotationClass)) {
+                listeMethode.add(methode);
+            }
+        }
+        return listeMethode;
+    }
+
+    public List<Class<?>> getClassesWithAnnotation(String NomPackage, Class<? extends Annotation> annotationClass) {
         List<Class<?>> listeClasse = getAllClasse(NomPackage);
         List<Class<?>> classWithAnnotation = new ArrayList<>();
         for (Class<?> classe : listeClasse) {
@@ -19,12 +98,12 @@ public class Utilitaire {
         return classWithAnnotation;
     }
 
-    public static List<Class<?>> getAllClasse(String NomPackage) {
+    public List<Class<?>> getAllClasse(String NomPackage) {
         List<Class<?>> listClass = new ArrayList<>();
         String path = NomPackage.replace(".", "/");
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         URL resource = classLoader.getResource(path);
-        if(resource == null) {
+        if (resource == null) {
             return listClass;
         }
         try {
